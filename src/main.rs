890a0b8af570dd0d71e4439a67dde6d70c6f6598
Richard Hozák <5235838+zxey@users.noreply.github.com>
2022-10-12 22:34:15 +0200

    Implement gate drawing and dragging

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,33 @@ use std::collections::HashMap;
 
 use macroquad::{hash, prelude::*, ui::root_ui};
 
-fn draw_gate(name: &str, x: f32, y: f32, w: f32, h: f32) {
-    let whitish = Color::from_rgba(0xcc, 0xcc, 0xcc, 0xff);
-    draw_rectangle(x, y, w, h, whitish);
+fn is_point_inside_box(
+    (point_x, point_y): (f32, f32),
+    (box_x, box_y, box_w, box_h): (f32, f32, f32, f32),
+) -> bool {
+    point_x > box_x && point_x < box_x + box_w && point_y > box_y && point_y < box_y + box_h
+}
+
+enum GateMouseHover {
+    Input(usize),
+    Output(usize),
+    Gate,
+}
+
+fn draw_gate(
+    name: &str,
+    x: f32,
+    y: f32,
+    inputs: &[bool],
+    outputs: &[bool],
+) -> Option<GateMouseHover> {
+    let max_io_len = usize::max(inputs.len(), outputs.len()) as f32;
+    let io_h = 20f32;
+    let io_w = 20f32;
+    let io_spacing = 5f32;
+    let h = max_io_len * io_h + max_io_len * io_spacing + io_spacing;
+    let w = h;
+
     let (font_size, font_scale, font_aspect) = camera_font_scale(h / 2.);
     let text_params = TextParams {
         font_size,
@@ -13,13 +37,55 @@ fn draw_gate(name: &str, x: f32, y: f32, w: f32, h: f32) {
         color: BLACK,
         ..Default::default()
     };
-    let dimensions = measure_text(name, None, font_size, font_scale);
+
+    let text_dimensions = measure_text(name, None, font_size, font_scale);
+
+    let whitish = Color::from_rgba(0xcc, 0xcc, 0xcc, 0xff);
+    draw_rectangle(x, y, w, h, whitish);
+
+    let mouse_pos = mouse_position();
+    let mut mouse_hover = None;
+
+    let dt = h / inputs.len() as f32;
+    for (index, state) in inputs.iter().enumerate() {
+        let t = 0.5 * dt + index as f32 * dt;
+        let in_x = x - io_w / 2.;
+        let in_y = y + t - (io_h / 2.);
+        draw_rectangle(in_x, in_y, io_w, io_h, if *state { RED } else { GRAY });
+
+        if is_point_inside_box(mouse_pos, (in_x, in_y, io_w, io_h)) {
+            mouse_hover = Some(GateMouseHover::Input(index));
+            draw_rectangle_lines(in_x, in_y, io_w, io_h, 4f32, WHITE);
+        }
+    }
+
+    let dt = h / outputs.len() as f32;
+    for (index, state) in outputs.iter().enumerate() {
+        let t = 0.5 * dt + index as f32 * dt;
+        let out_x = x + w - io_w / 2.;
+        let out_y = y + t - (io_h / 2.);
+        draw_rectangle(out_x, out_y, io_w, io_h, if *state { RED } else { GRAY });
+
+        if is_point_inside_box(mouse_pos, (out_x, out_y, io_w, io_h)) {
+            mouse_hover = Some(GateMouseHover::Output(index));
+            draw_rectangle_lines(out_x, out_y, io_w, io_h, 4f32, WHITE);
+        }
+    }
+
     draw_text_ex(
         name,
-        x + (w - dimensions.width) / 2.,
-        y + (h - dimensions.height) / 2. + dimensions.offset_y,
+        x + (w - text_dimensions.width) / 2.,
+        y + (h - text_dimensions.height) / 2. + text_dimensions.offset_y,
         text_params,
     );
+
+    if mouse_hover.is_some() {
+        mouse_hover
+    } else if is_point_inside_box(mouse_pos, (x, y, w, h)) {
+        Some(GateMouseHover::Gate)
+    } else {
+        None
+    }
 }
 
 trait Gate<const INPUTS: usize, const OUTPUTS: usize> {
@@ -35,6 +101,16 @@ impl Gate<2, 1> for And {
 
     fn update(&self, inputs: &[bool; 2], outputs: &mut [bool; 1]) {
         outputs[0] = inputs[0] && inputs[1];
+    }
+}
+
+struct And3;
+
+impl Gate<3, 1> for And3 {
+    const NAME: &'static str = "AND3";
+
+    fn update(&self, inputs: &[bool; 3], outputs: &mut [bool; 1]) {
+        outputs[0] = inputs[0] && inputs[1] && inputs[2];
     }
 }
 
@@ -124,6 +200,11 @@ impl Simulation {
         self.connections.push((from, output, to, input));
     }
 
+    fn get_gate_state(&self, id: usize) -> (&[bool], &[bool]) {
+        let gate = self.gates.get(&id).unwrap();
+        (&gate.inputs, &gate.outputs)
+    }
+
     fn simulate(&mut self) {
         for (from, output, to, input) in &self.connections {
             let output_state = self.gates.get(from).unwrap().outputs[*output];
@@ -140,12 +221,28 @@ impl Simulation {
 async fn main() {
     let mut sim = Simulation::new();
     sim.add_gate(And);
+    sim.add_gate(Or);
+    sim.add_gate(Not);
+    sim.add_gate(Xor);
+    sim.add_gate(And3);
+    let mut board_gates = Vec::<(usize, &'static str, f32, f32)>::new();
+    board_gates.push((0, "AND", 200., 0.));
+    board_gates.push((1, "OR", 250., 0.));
+    board_gates.push((2, "NOT", 300., 0.));
+    board_gates.push((3, "XOR", 350., 0.));
+    board_gates.push((4, "AND3", 450., 0.));
+
+    let mut dragging: Option<usize> = None;
 
     let blackish = Color::from_rgba(0x1e, 0x1e, 0x1e, 0xff);
     let mut last_update = get_time();
     let mut frequency = 10f32;
     let mut elapsed_remainder = 0f64;
     loop {
+        if is_mouse_button_released(MouseButton::Left) && dragging.is_some() {
+            dragging = None;
+        }
+
         clear_background(blackish);
 
         let period = (1.0 / frequency) as f64;
@@ -159,21 +256,47 @@ async fn main() {
 
             // println!("{:.5} {:.5} {:.5} {:.5} {:<5} {:.5}", elapsed, period, elapsed / period, elapsed % period, iterations, elapsed_remainder);
 
-            println!("iterations {}", iterations);
+            // println!("iterations {}", iterations);
             for _ in 0..iterations {
-                println!("tick");
+                // println!("tick");
                 sim.simulate();
             }
         }
 
-        draw_gate("AND", 0., 0., 50.0, 50.0);
-        draw_gate("OR", 50., 0., 50.0, 50.0);
-        draw_gate("NOT", 100., 0., 50.0, 50.0);
-        draw_gate("XOR", 150., 0., 50.0, 50.0);
+        for &mut (id, name, ref mut x, ref mut y) in &mut board_gates {
+            if let Some(dragging_id) = dragging {
+                if dragging_id == id {
+                    let pos = mouse_position();
+                    println!("setting pos {:?}", pos);
 
-        // root_ui().window(hash!(), vec2(0.0, 0.0), vec2(200.0, 400.0), |ui| {
-        //     ui.slider(hash!(), "Frequency Hz", 1f32..100f32, &mut frequency);
-        // });
+                    *x = pos.0;
+                    *y = pos.1;
+                }
+            }
+
+            let (inputs, outputs) = sim.get_gate_state(id);
+            if let Some(mouse_hover) = draw_gate(name, *x, *y, inputs, outputs) {
+                match mouse_hover {
+                    GateMouseHover::Input(input_id) => {
+                        println!("input id {}", input_id);
+                    },
+                    GateMouseHover::Output(output_id) => {
+                        println!("output id {}", output_id);
+                    },
+                    GateMouseHover::Gate => {
+                        if dragging.is_none() {
+                            if is_mouse_button_pressed(MouseButton::Left) {
+                                dragging = Some(id);
+                            }
+                        }
+                    },
+                }
+            }
+        }
+
+        root_ui().window(hash!(), vec2(0.0, 0.0), vec2(200.0, 400.0), |ui| {
+            ui.slider(hash!(), "Frequency Hz", 1f32..100f32, &mut frequency);
+        });
 
         next_frame().await
     }
